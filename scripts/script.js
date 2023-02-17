5b32f9707ba41d466561515fe0d3db63d3f362c5
Niladri Das <86772172+SegFault03@users.noreply.github.com>
2023-02-17 06:27:06 +0000

    added a loading dialog

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const result = document.getElementById('result');
 const form = document.getElementById('form');
 const result_div = document.getElementById('result-div');
+const result_title = document.getElementById('result-title');
 form.addEventListener("submit",getData);
 result_div.style.display = "none";
 var myHeaders = new Headers();
@@ -14,6 +15,9 @@ var requestOptions = {
 
 function getData(event)
 {
+  result_div.style.display = "block";
+  result_title.innerText = "Loading...";
+  result.style.display = "none";
     let age = document.getElementById('age').value;
     let bs = document.getElementById('bs').value;
     let sbp = document.getElementById('sbp').value;
@@ -26,6 +30,6 @@ function getData(event)
     fetch(`https://zxv5hi.deta.dev/predict?sample=${dataJson}`, requestOptions)
   .then(response => response.text())
   .then(prediction => {result.innerText=prediction.substring(2,prediction.lastIndexOf('"'));
-  result_div.style.display = "block";})
+  result.style.display = "block";result_title.innerText = "Here's your prediction!";})
   .catch(error => console.log('error', error));
 }
