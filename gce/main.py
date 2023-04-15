e65cf1dd2e6f149f6b4542ef82cf349425842465
Niladri Das <boney0310@gmail.com>
2023-04-15 19:50:23 +0530

    prepped for testing deployment to gce

---
+++ gce/main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import joblib
+import json
+import numpy
+
+
+model = joblib.load('models/pregnancy_risk_classifier.pkl')
+scaler = joblib.load('models/scaler.pkl')
+
+app = Flask(__name__)
+
+@app.route('/predict/<string:sample>', methods=['GET'])
+def getPrediction(sample) -> str:
+    data = json.loads(sample)["data"]
+    data = numpy.array(data)
+    data = scaler.transform(data.reshape(1,-1))
+    result = model.predict(data)
+    return result.tolist()
+
+@app.route('/', methods=['GET'])
+def wakeUp() -> str:
+    return "Hey!"
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
